01082c73f67e1b56cc3ae560e532ba8f5655d796
Tony Jeffree <tjeffree@gmail.com>
2014-01-17 14:37:29 +0000

    Updated skills and implemented

--- assets/WebDev.class.php
+++ assets/WebDev.class.php
@@ -66,7 +66,8 @@ class Position_Web_Developer extends Position {
         'web application to notify of PHP errors on live servers - WebSockets / Redis pubsub / Javascript / HTML',
         'distributed report generation queue - Redis / PHP Daemons',
         'MVC framework for all new web applications (in use on tachomaster.co.uk, falcontracking.co.uk, predrive.co.uk)',
-        'created deployment web app for internal use to interface with current rsync set up to move code to the staging server',
+        'set up Grunt tasks for unit testing, packaging, versioning and deployment to staging server',
+        'created Knowledge Base for user-end documentation, taking advantage of docpad for static site generation',
         'autoloading of common classes across web applications to make a DRYer environment',                 // DRY = Don`t Repeat Yourself
         'email ticketing system for support department',
         'Google Analytics across all web applications',
@@ -75,8 +76,7 @@ class Position_Web_Developer extends Position {
         'general notification application for web services - Adobe Air / Flex',
         'SVN version control',
         'bug, feature and enhancement tracking application',
-        'Android and iPhone/iPad App utilising my core skills with PhoneGap/Cordova/AngularJS',
-        'Java J2ME mobile phone application for recording working time'
+        'Android and iPhone/iPad App utilising my core skills with PhoneGap/Cordova/AngularJS'
     );
 
 }
@@ -115,7 +115,7 @@ class TonyJeffree extends Web_Developer {
 
     public function __construct() {
         
-		date_default_timezone_set('Europe/London');
+        date_default_timezone_set('Europe/London');
 
         $this->gatherCoreSkills();
         $this->gatherSkills();
@@ -128,21 +128,21 @@ class TonyJeffree extends Web_Developer {
         
     }
 
-	function date_diff($date1, $date2) { 
-		$current = $date1; 
-		$datetime2 = date_create($date2); 
-		$count = 0; 
-		while(date_create($current) < $datetime2) { 
-			$current = gmdate("Y-m-d", strtotime("+1 year", strtotime($current))); 
-			$count++;
-		}
-		return $count; 
-	} 
+    function date_diff($date1, $date2) { 
+        $current = $date1; 
+        $datetime2 = date_create($date2); 
+        $count = 0; 
+        while(date_create($current) < $datetime2) { 
+            $current = gmdate("Y-m-d", strtotime("+1 year", strtotime($current))); 
+            $count++;
+        }
+        return $count; 
+    } 
 
     protected function gatherCoreSkills() {
         
         $now = new DateTime('now');
-		$fmt = $now->format('Y-m-d');
+        $fmt = $now->format('Y-m-d');
 
         $this->exp['Javascript']       = $this->date_diff('2001-01-01', $fmt);
         $this->exp['HTML']             = $this->date_diff('2001-01-01', $fmt);
@@ -152,21 +152,21 @@ class TonyJeffree extends Web_Developer {
 
     protected function gatherSkills() {
         
-        $this->skills[]                = 'jQuery';
+        $this->skills[]                = 'CSS (2, 3, ..)';
+        $this->skills[]                = 'node.js';
         $this->skills[]                = 'MySQL';
-        $this->skills[]                = 'CSS2, CSS3';                                    // CSS3 included!
+        $this->skills[]                = 'Version control (SVN, Git)';
+        $this->skills[]                = 'jQuery, modernizr, Bootstrap';
         $this->skills[]                = 'Linux (command line, scripting)';
-        $this->skills[]                = 'nodejs';
         $this->skills[]                = 'Redis';
         $this->skills[]                = 'Java';
         $this->skills[]                = 'C#';
         $this->skills[]                = 'DNS';
         $this->skills[]                = 'AngularJS';
         $this->skills[]                = 'PhoneGap / Cordova';
-        $this->skills[]                = 'Android Deployment';
+        $this->skills[]                = 'Android/iOS Deployment';
         $this->skills[]                = 'Photoshop';
-        $this->skills[]                = 'SVN';
-        $this->skills[]                = 'Git';
+        $this->skills[]                = 'Touch typing (100 wpm on a good day)';
 
     }
 
@@ -176,7 +176,7 @@ class TonyJeffree extends Web_Developer {
         $this->websites[]             = array('Falcon', 'http://www.falcontracking.co.uk');
         $this->websites[]             = array('Road Tech', 'http://www.roadtech.co.uk');
         $this->websites[]             = array('Word Game', 'http://shex.co.uk/wordgame/');
-        $this->websites[]             = array('Tachomaster Worker App (Google Play)', 'https://play.google.com/store/apps/details?id=com.RoadTech.Tachomaster');
+        $this->websites[]             = array('Tachomaster Worker App (Google Play linked, also on iOS)', 'https://play.google.com/store/apps/details?id=com.RoadTech.Tachomaster');
 
     }
 
